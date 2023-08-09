0a88c1f6163df807e3e05bc5244d8ee06c45e890
Arturo Zepeda <aaze92@gmail.com>
2023-08-08 21:16:44 -0600

    inicializo proyecto e incluyo instalacion de componentes de node

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <h1 class="text-6xl text-center text-blue-500 bg-slate-900">Hello Vite + Vue 3 + TailWind!</h1>
+</template>
+
+
